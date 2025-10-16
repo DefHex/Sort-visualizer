0ed2d21cfb1e959b58c83a4d40c438e56c199fb1
SadeghAllahverdi <sadegh7644@gmail.com>
2025-10-16 17:42:47 +0200

    Building the logic and a mock up in python and customtkinter...

--- python/merge_sort.py
+++ python/merge_sort.py
@@ -20,30 +20,3 @@ def merge_sort(arr):
     left = merge_sort(arr[:mid])
     right = merge_sort(arr[mid:])
     return merge(left, right)
-
-
-if __name__ == "__main__":
-    import time
-
-    raw_input = input(
-        "please enter an array of (5 - 10) elements between (0 - 555) seperated by (,)"
-    )
-    raw_input.strip()
-    if not raw_input:
-        print("Please enter some numbers!")
-    try:
-        arr = [int(x.strip()) for x in raw_input.split(",")]
-    except ValueError:
-        print("Please make sure to seperate the elements using (,)")
-
-    if any(x < 0 or x > 555 for x in arr):
-        print("Please make sure to enter numbers between 0 and 555")
-
-    if len(arr) < 5 or len(arr) > 10:
-        print("array should have minimum 5 and maximum 10 elements")
-
-    start = time.time()
-    merge_sort(arr)
-    end = time.time()
-
-    print(f"Array: {arr} \nRun Time: {(end - start) * 1000:.2f} ms")
--- python/quick_sort.py
+++ python/quick_sort.py
@@ -16,30 +16,3 @@ def quick_sort(arr, low=0, high=None):
         pindex = quick(arr, low, high)
         quick_sort(arr, low, pindex - 1)
         quick_sort(arr, pindex + 1, high)
-
-
-if __name__ == "__main__":
-    import time
-
-    raw_input = input(
-        "please enter an array of (5 - 10) elements between (0 - 555) seperated by (,)"
-    )
-    raw_input.strip()
-    if not raw_input:
-        print("Please enter some numbers!")
-    try:
-        arr = [int(x.strip()) for x in raw_input.split(",")]
-    except ValueError:
-        print("Please make sure to seperate the elements using (,)")
-
-    if any(x < 0 or x > 555 for x in arr):
-        print("Please make sure to enter numbers between 0 and 555")
-
-    if len(arr) < 5 or len(arr) > 10:
-        print("array should have minimum 5 and maximum 10 elements")
-
-    start = time.time()
-    quick_sort(arr)
-    end = time.time()
-
-    print(f"Array: {arr} \nRun Time: {(end - start) * 1000:.2f} ms")
